a7151525be874ec8839f58af0945beb8e12b62fc
carson <wanganlin@ecmoban.com>
2018-03-12 10:45:19 +0800

    optimize

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * ECTouch - E-Commerce Platform for PHP.
+ *
+ * @package  ECTouch
+ * @homepage https://www.ectouch.cn
+ */
+
 // 加载基础文件
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
