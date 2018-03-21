dc02077f043357df91d2f852f0bec0fb388fba50
carson <wanganlin@ecmoban.com>
2018-03-21 12:17:09 +0800

    增加字符串转换函数

--- app/support/helpers.php
+++ app/support/helpers.php
@@ -92,6 +92,41 @@ function asset($url = null)
 }
 
 /**
+ * 将指定的字符串转换成 驼峰式命名
+ * Translates a string with underscores
+ * into camel case (e.g. first_name -> firstName)
+ *
+ * @param string $str String in underscore format
+ * @param bool $capitalise_first_char If true, capitalise the first char in $str
+ * @return string $str translated into camel caps
+ */
+function camel_case($str, $capitalise_first_char = false)
+{
+    if ($capitalise_first_char) {
+        $str[0] = strtoupper($str[0]);
+    }
+    return preg_replace_callback('/_([a-z])/', function ($c) {
+        return strtoupper($c[1]);
+    }, $str);
+}
+
+/**
+ * 将指定的字符串转换成 蛇形命名
+ * Translates a camel case string into a string with
+ * underscores (e.g. firstName -> first_name)
+ *
+ * @param string $str String in camel case format
+ * @return string $str Translated into underscore format
+ */
+function snake_case($str)
+{
+    $str[0] = strtolower($str[0]);
+    return preg_replace_callback('/([A-Z])/', function ($c) {
+        return "_" . strtolower($c[1]);
+    }, $str);
+}
+
+/**
  * 是否为移动设备
  * @return mixed
  */
