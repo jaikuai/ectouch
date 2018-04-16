21f31386787681f5f1994966600e1bafef89980b
carson <wanganlin@ecmoban.com>
2018-04-16 18:26:10 +0800

    更新服务

---
+++ app/install/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\install\controllers;
+
+/**
+ * Class IndexController
+ * @package app\install\controllers
+ */
+class IndexController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return 'install';
+    }
+}
---
+++ app/services/article/CategoryService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\services\article;
+
+use app\repositorys\ArticleRepository;
+use app\contracts\services\article\CategoryInterface;
+
+class CategoryService implements CategoryInterface
+{
+
+}
