9964f26018a0d551324c1499a07e145f70cc35a9
carson <wanganlin@ecmoban.com>
2018-04-16 16:05:03 +0800

    upgrade helper

--- app/support/helpers.php
+++ app/support/helpers.php
@@ -81,14 +81,188 @@ function plugin_path($path = '')
 }
 
 /**
- * 返回资源URL
- * @param null $url
+ * 获取应用主体
+ * @param null $component
+ * @return bool|mixed
+ */
+function app($component = null)
+{
+    if (!is_null($component) && isset(Yii::$app->{$component})) {
+        return Yii::$app->{$component};
+    }
+
+    return false;
+}
+
+/**
+ * Get / set the specified configuration value.
+ *
+ * If an array is passed as the key, we will assume you want to set an array of values.
+ *
+ * @param  array|string $key
+ * @param  mixed $default
+ * @return mixed
+ */
+function config($key = null, $default = null)
+{
+    /*    if (is_null($key)) {
+            return app('config');
+        }
+        if (is_array($key)) {
+            return app('config')->set($key);
+        }
+        return app('config')->get($key, $default);*/
+    static $_config = [];
+    $item = 'params';
+    // 指定参数来源
+    if (strpos($key, '.')) {
+        list($item, $key) = explode('.', $key, 2);
+    }
+    if (!isset($_config[$item])) {
+        $_config[$item] = require config_path($item . '.php');
+    }
+    return isset($_config[$item][$key]) ? $_config[$item][$key] : null;
+}
+
+/**
+ * Get / set the specified cache value.
+ *
+ * If an array is passed, we'll assume you want to put to the cache.
+ *
+ * @param  dynamic  key|key,default|data,expiration|null
+ * @return mixed
+ *
+ * @throws \Exception
+ */
+function cache()
+{
+    $arguments = func_get_args();
+    if (empty($arguments)) {
+        return app('cache');
+    }
+    if (is_string($arguments[0])) {
+        return app('cache')->get($arguments[0], isset($arguments[1]) ? $arguments[1] : null);
+    }
+    if (!is_array($arguments[0])) {
+        throw new Exception(
+            'When setting a value in the cache, you must pass an array of key / value pairs.'
+        );
+    }
+    if (!isset($arguments[1])) {
+        $arguments[1] = 0;
+    }
+    return app('cache')->set(key($arguments[0]), reset($arguments[0]), $arguments[1]);
+}
+
+/**
+ * Get / set the specified session value.
+ *
+ * If an array is passed as the key, we will assume you want to set an array of values.
+ *
+ * @param  array|string $key
+ * @param  mixed $default
+ * @return mixed
+ */
+function session($key = null, $default = null)
+{
+    if (is_null($key)) {
+        return app('session');
+    }
+    if (is_array($key)) {
+        return app('session')->set(key($key), reset($key));
+    }
+    return app('session')->get($key, $default);
+}
+
+/**
+ * Create a new cookie instance.
+ *
+ * @param null $name
+ * @param null $value
+ * @param int $minutes
+ * @param null $path
+ * @param null $domain
+ * @param bool $secure
+ * @param bool $httpOnly
+ * @return mixed
+ */
+function cookie($name = null, $value = null, $minutes = 0, $path = null, $domain = null, $secure = false, $httpOnly = true)
+{
+    $cookie = app('request')->cookies;
+    if (is_null($name)) {
+        return $cookie->getValue($name);
+    }
+
+    return $cookie->add(new \yii\web\Cookie([
+        'name' => $name,
+        'value' => $value,
+        'expire' => $minutes,
+        'path' => $path,
+        'domain' => $domain,
+        'secure' => $secure,
+        'httpOnly' => $httpOnly
+    ]));
+}
+
+/**
  * @return string
  */
-function asset($url = null)
+function csrf_field()
 {
-    $url = is_null($url) ? '' : '/' . ltrim($url, '/');
-    return request()->rootUrl() . $url;
+    $request = app('request');
+    return '<input type="hidden" name="' . $request->csrfParam . '" value="' . $request->getCsrfToken() . '" />';
+}
+
+/**
+ * @return array
+ */
+function input($name = '', $default = null)
+{
+    return Yii::$app->request->get($name, $default);
+}
+
+/**
+ * @param $url
+ * @param array $param
+ * @return string
+ */
+function url($url, $param = [])
+{
+    return yii\helpers\Url::toRoute(array_merge([$url], $param));
+}
+
+/**
+ * Generate the URL to an application asset.
+ *
+ * @param  string $path
+ * @return string
+ */
+function asset($path = '/')
+{
+    if (is_valid_url($path)) {
+        return $path;
+    }
+    // Once we get the root URL, we will check to see if it contains an index.php
+    // file in the paths. If it does, we will remove it since it is not needed
+    // for asset paths, but only for routes to endpoints in the application.
+    $root = yii\helpers\Url::home();
+    $root = str_replace('/index.php', '', $root);
+
+    return rtrim($root, '/') . '/' . trim($path, '/');
+}
+
+/**
+ * Determine if the given path is a valid URL.
+ *
+ * @param  string $path
+ * @return bool
+ */
+function is_valid_url($path)
+{
+    if (!preg_match('~^(#|//|https?://|mailto:|tel:)~', $path)) {
+        return filter_var($path, FILTER_VALIDATE_URL) !== false;
+    }
+    return true;
 }
 
 /**
@@ -132,7 +306,8 @@ function snake_case($str)
  */
 function is_mobile_device()
 {
-    return request()->isMobile();
+    $detect = new \Mobile_Detect();
+    return $detect->isMobile();
 }
 
 /**
@@ -148,8 +323,7 @@ function load_helper($files = [], $module = '')
     if (empty($module)) {
         $base_path = app_path('helpers/');
     } else {
-        $module = ($module == 'admin') ? 'dashboard' : $module;
-        $base_path = app_path(ucfirst($module) . '/common/');
+        $base_path = app_path($module . '/common/');
     }
     foreach ($files as $vo) {
         $helper = $base_path . $vo . '.php';
@@ -173,8 +347,7 @@ function load_lang($files = [], $module = '')
     if (empty($module)) {
         $base_path = resource_path('lang/' . $GLOBALS['_CFG']['lang'] . '/');
     } else {
-        $module = ($module == 'admin') ? 'dashboard' : $module;
-        $base_path = app_path(ucfirst($module) . '/lang/' . $GLOBALS['_CFG']['lang'] . '/');
+        $base_path = app_path($module . '/lang/' . $GLOBALS['_CFG']['lang'] . '/');
     }
     foreach ($files as $vo) {
         $helper = $base_path . $vo . '.php';
@@ -191,16 +364,34 @@ function load_lang($files = [], $module = '')
 
 /**
  * 浏览器友好的变量输出
- * @param $var
- * @param bool $echo
- * @param null $label
- * @return mixed|null|string|string[]
+ * @param mixed $var 变量
+ * @param boolean $echo 是否输出 默认为True 如果为false 则返回输出字符串
+ * @param string $label 标签 默认为空
+ * @param boolean $strict 是否严谨 默认为true
+ * @return void|string
  */
-function dd($var, $echo = true, $label = null)
+function dd($var, $echo = true, $label = null, $strict = true)
 {
-    if ($echo) {
-        dump($var, $echo, $label);
+    $label = ($label === null) ? '' : rtrim($label) . ' ';
+    if (!$strict) {
+        if (ini_get('html_errors')) {
+            $output = print_r($var, true);
+            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        } else {
+            $output = $label . print_r($var, true);
+        }
     } else {
-        return dump($var, $echo, $label);
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        if (!extension_loaded('xdebug')) {
+            $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        }
+    }
+    if ($echo) {
+        die($output);
+    } else {
+        return $output;
     }
 }
